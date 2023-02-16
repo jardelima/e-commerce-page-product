0d914a02ead4fc254961f76bf1f3da1ca9374944
Jardel Lima <prof_jardel@hotmail.com>
2023-02-16 10:25:44 -0300

    Adicionar funcão para adicionar produto ao carrinho
    - Adicionar funcão para adicionar produto ao carrinho.

--- src/js/modules/add-product-cart.js
+++ src/js/modules/add-product-cart.js
@@ -1,14 +1,16 @@
 export default function addProductCart() {
     if(document.querySelector(".add-product-cart")) {
         let btnAddProductCart = document.querySelector(".add-product-cart");
+        let imgProduct = document.querySelector(".images-products .img-product img").getAttribute("src");
         let nameProduct = document.querySelector(".description h2").innerHTML;
-        let priceProduct = document.querySelector(".price-product").innerText.split("\n");
         let btnCart = document.querySelectorAll(".icon-cart");
         let qtdProductCart = document.querySelector(".cart-active");
+        let modalCart = document.querySelector("[data-modal='modal-cart'] .modal__box");
         let cart = [];
 
         btnAddProductCart.addEventListener("click", () => {
             let qtdProduct = Number(document.querySelector(".qtd").innerHTML);
+            let priceProduct = document.querySelector(".price-product span").innerHTML;
 
             if(qtdProduct > 0) {
                 if(cart.length > 0) {
@@ -17,15 +19,16 @@ export default function addProductCart() {
                             cart.push({
                                 qtdProduct: qtdProduct,
                                 nameProduct: nameProduct,
-                                priceProduct: priceProduct[0],
+                                priceProduct: priceProduct,
                             });
                         }
                     });
                 } else {
                     cart.push({
+                        imgProduct: imgProduct,
                         qtdProduct: qtdProduct,
                         nameProduct: nameProduct,
-                        priceProduct: priceProduct[0],
+                        priceProduct: priceProduct,
                     });
                 }
 
@@ -36,5 +39,42 @@ export default function addProductCart() {
                 qtdProductCart.innerHTML = cart.length;
             }
         });
+
+        btnCart.forEach(btn => {
+            btn.addEventListener("click", () => {
+                let productCart = document.createElement("div");
+
+                if(cart.length > 0) {
+                    if(document.querySelector(".default-message")) {
+                        let defaultMessage = document.querySelector(".default-message");
+                        defaultMessage.classList.add("disabled");
+                    }
+
+                    cart.map((product) => {
+                        return (
+                            productCart.innerHTML = `
+                                <div class="product-cart">
+                                    <div class="product-cart__image">
+                                        <img src="${product.imgProduct}" alt="Imagem do produto" />
+                                    </div>
+
+                                    <div class="product-cart__description">
+                                        <h4>${product.nameProduct}</h4>
+                                        <div class="product-cart__qtd"><span>Quantidade:</span> ${product.qtdProduct}</div>
+                                        <div class="product-cart__price"><span>Preço:</span> ${product.priceProduct}</div>
+                                    </div>
+                                </div>
+                            `
+                        )
+                    });
+                } else {
+                    productCart.innerHTML = `
+                        <p class="default-message">Nenhum produto adicionado ao carrinho</p>
+                    `
+                }
+
+                modalCart.append(productCart)
+            })
+        })
     }
 }
--- src/js/modules/quantity-product.js
+++ src/js/modules/quantity-product.js
@@ -3,19 +3,27 @@ export default function quantityProduct() {
         let sub = document.querySelector(".sub");
         let qtd = document.querySelector(".qtd");
         let amount = document.querySelector(".amount");
+        let price = document.querySelector(".price-product span");
 
+        let cleanPrice = Number(price.innerHTML.replace(",", "."));
         let cleanQtd = Number(qtd.innerHTML);
 
+        console.log(cleanPrice)
+
         sub.addEventListener("click", () => {
-            if(cleanQtd >= 0) {
+            if(cleanQtd > 1) {
                 cleanQtd--;
                 qtd.innerHTML = cleanQtd;
+                let result = cleanPrice * cleanQtd;
+                price.innerHTML = result.toLocaleString("pt-br", {minimumFractionDigits: 2});
             }
         });
 
         amount.addEventListener("click", () => {
             cleanQtd++;
             qtd.innerHTML = cleanQtd;
+            let result = cleanPrice * cleanQtd;
+            price.innerHTML = result.toLocaleString("pt-br", {minimumFractionDigits: 2});
         });
     }
 }
